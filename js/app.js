d712aad96f7102592880ec3465149fb2f34bd4be
Thiha Aung <thihaaung122122@gmail.com>
2022-07-03 01:19:08 +0630

    3/7/2022

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
         $(".ui").addClass("useri");
         $(".menus").removeClass("menuicons");
         $(".menuimgs").hide();
+        $(".firstj").addClass("first");
+        $(".boxs").addClass("heros");
+    });
+
+    $(".menuitems").click(function(){
+        $(this).addClass("activeitems").siblings().removeClass("activeitems");
     });
 
 });
