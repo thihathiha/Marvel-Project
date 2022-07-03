09ddb63abdece2e2d3e448c6acc9589df676ea37
Thiha Aung <thihaaung122122@gmail.com>
2022-07-04 00:19:20 +0630

    4/7/2022

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
 
     $(".menus").click(function(){
+        // for herobg 
         $(".ui").addClass("useri");
+        // remove avenger icon and text
         $(".menus").removeClass("menuicons");
         $(".menuimgs").hide();
+        // add black background 
         $(".firstj").addClass("first");
+        // opacity 1 for heros 
         $(".boxs").addClass("heros");
     });
 
     $(".menuitems").click(function(){
         $(this).addClass("activeitems").siblings().removeClass("activeitems");
+
+        // for roles 
+        let getattvalue = $(this).attr("data-filter");
+        // console.log(getattvalue);
+
+        if (getattvalue === "all"){
+            $(".roles").show();
+        }else{
+            $(".roles").hide();
+            $(".roles").filter("." + getattvalue).show();
+        }
     });
 
 });
